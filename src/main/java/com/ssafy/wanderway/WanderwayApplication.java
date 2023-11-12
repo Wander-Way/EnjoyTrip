a6e857410be6c3a0eb3a70d9eb807fe39a3142c4
cheshireHYUN <seohyun@chungbuk.ac.kr>
2023-11-13 02:39:27 +0900

    [Feat] 스프링부트 기본설정

---
+++ src/main/java/com/ssafy/wanderway/WanderwayApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.wanderway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WanderwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WanderwayApplication.class, args);
+	}
+
+}
