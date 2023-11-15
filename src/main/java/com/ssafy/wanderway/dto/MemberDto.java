e6760cd5b64b69b703e5ebb1d8b1c1e967f27416
류진호 <dbwlsgh57@naver.com>
2023-11-15 16:45:44 +0900

    [Feat] 맴버 회원 정보 조회, 정보 수정,회원 가입 완성

--- src/main/java/com/ssafy/wanderway/dto/MemberDto.java
+++ src/main/java/com/ssafy/wanderway/dto/MemberDto.java
@@ -1,4 +1,30 @@
 package com.ssafy.wanderway.dto;
 
+import com.ssafy.wanderway.domain.Member;
+import lombok.*;
+
+//@Data
+@Getter
+@NoArgsConstructor
+@ToString
+@Setter
+//@ApiModel(value   = "MemberDto : 맴버 정보")
 public class MemberDto {
+
+  private Long id;//생략
+  private String email;
+  private String password;
+  private String nickname;
+  private String profile;
+
+
+
+  public MemberDto (Member member){
+    this.email = member.getEmail();
+    this.password = member.getPassword(); 
+    this.nickname = member.getNickname(); 
+    this.profile = member.getProfile(); 
+  }
+
+
 }
